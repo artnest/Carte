a2e523c11c9357bce6f44c77f1ee06622afb7783
artnest <artem291997@gmail.com>
2017-11-28 12:16:26 +0300

    add cities viewmodel

--- app/src/main/java/by/carte/restaurants/ui/cities/CitiesActivity.kt
+++ app/src/main/java/by/carte/restaurants/ui/cities/CitiesActivity.kt
@@ -6,9 +6,20 @@ import by.carte.restaurants.R
 
 class CitiesActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: CitiesViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cities)
+
+        // setup toolbar
+
+        setupViewFragment()
+
+        viewModel = obtainViewModel().apply {
+            openCityEvent.observe() {
+            }
+        }
 
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, CitiesFragment.newInstance())
